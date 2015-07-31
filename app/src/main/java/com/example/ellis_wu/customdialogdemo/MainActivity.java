a8bbf491bf5f8e2b855dc18856b2fd252affc197
elliswu <kill823@gmail.com>
2015-07-31 14:56:46 +0800

    first commit

---
+++ app/src/main/java/com/example/ellis_wu/customdialogdemo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.ellis_wu.customdialogdemo;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+    private Button btn_open;
+    private Button btn_delay_open;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn_open = (Button)findViewById(R.id.btn_open);
+        btn_delay_open = (Button)findViewById(R.id.btn_delay_open);
+
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CustomDialogActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_delay_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CustomDialogActivity.class);
+                try {
+                    Thread.sleep(5000);
+                    startActivity(intent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
